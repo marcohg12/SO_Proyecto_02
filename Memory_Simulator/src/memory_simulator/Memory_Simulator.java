e699c4f336499f6299c9178ff03326389a4e950b
Arianaam335 <arianaam335@estudiantec.cr>
2024-10-09 09:34:17 -0600

    a ver x2

--- Memory_Simulator/src/memory_simulator/Memory_Simulator.java
+++ Memory_Simulator/src/memory_simulator/Memory_Simulator.java
@@ -2,14 +2,19 @@ package memory_simulator;
 
 import java.util.ArrayList;
 import memory_simulator.logic.Simulation;
+import memory_simulator.logic.InstructionSetGenerator;
 import memory_simulator.model.ComputerState;
 import memory_simulator.model.PaginationAlgoType;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.List;
 
 public class Memory_Simulator {
 
     public static void main(String[] args) {
         
-        ArrayList<String> instructions = new ArrayList();
+        /*ArrayList<String> instructions = new ArrayList();
         instructions.add("new(1,250)");
         instructions.add("new(1,50)");
         instructions.add("new(2,5320)");
@@ -27,6 +32,22 @@ public class Memory_Simulator {
             ComputerState state = simulation.getState();
             i += 1;
             System.out.println(i);
+        }*/
+        
+        int seed = 2;
+        int numProcesses = 10;
+        int numOperations = 500;
+
+        // Crear una instancia del generador de instrucciones
+        InstructionSetGenerator generator = new InstructionSetGenerator(seed, numProcesses, numOperations);
+        
+        // Generar las instrucciones y escribirlas en "instructions.txt"
+        List<String> instrucciones = generator.generateInstructions();
+        
+        // Mostrar las instrucciones generadas
+        System.out.println("Instrucciones generadas:");
+        for (String instruccion : instrucciones) {
+            System.out.println(instruccion);
         }
     }
     
